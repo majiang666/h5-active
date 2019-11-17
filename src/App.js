fe85bb9072c02ff109edf3dd42b95a52eee6297d
cssking <laji198888@163.com>
2019-11-17 18:29:50 +0800

    优化活动

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './assets/css/index.less';
 import img from './assets/img/activity.png';
 
+const openAlipay = () => {
+  window.location.href = 'https://www.alipay.com';
+}
 const App = () => {
   return (
     <div className="main">
 
-      <img src={img} width="100%" />
+      <img src={ img } width="100%" />
 
       <a href="https://www.baidu.com" target="_blank" className="area1"></a>
       <a href="https://www.qq.com" target="_blank" className="area2"></a>
       <a href="https://www.163.com" target="_blank" className="area3"></a>
 
-      <a href="https://www.baidu.com" target="_blank" className="area4"></a>
-      <a href="https://www.baidu.com" target="_blank" className="area5"></a>
-
+      <a href="#" target="_blank" className="area4">
+        <p className="area5" onClick={ openAlipay }></p>
+      </a>
 
     </div>
   )
